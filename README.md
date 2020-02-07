3b0599fe4ae1e8669a0f9d8eeb3332842970176f
Andrei Plamada <andrei.plamada@id.ethz.ch>
2020-02-07 14:38:35 +0100

    refactor word2vec

--- README.md
+++ README.md
@@ -20,3 +20,17 @@ sys.path.append("narrative-nlp/code")
 
 from word_embedding import preprocess
 ```
+## Development
+- use [black](https://black.readthedocs.io/en/stable/
+
+### Tools
+- Auto-formatter: [black](https://black.readthedocs.io/en/stable/) that might break the [79 characters maximum line length](https://www.python.org/dev/peps/pep-0008/#maximum-line-length) from PEP8, see [here](https://github.com/psf/black#line-length)
+- Testing via [pytest](https://docs.pytest.org/en/latest/) with `--doctest-modules` enabled (see [doctest](http://doc.pytest.org/en/latest/doctest.html))
+- security issues: [bandit](https://github.com/PyCQA/bandit)
+- [mypy](http://mypy-lang.org/)
+- [pycodestyle](https://github.com/PyCQA/pycodestyle), [pydocstyle](https://github.com/PyCQA/pydocstyle), [Pylint](https://github.com/PyCQA/pylint), [flake8](https://gitlab.com/pycqa/flake8)
+
+### Guides
+- [PEP8](https://www.python.org/dev/peps/pep-0008/)
+- [Coding Style](http://google.github.io/styleguide/pyguide.html)
+- Docstring using Google Style: see [Sphinx 1.3 Google Style Python Docstrings](https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html) and [pyguide](http://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings)
--- code/word_embedding.py
+++ code/word_embedding.py
@@ -1,33 +1,94 @@
 import re
 import string
-from typing import List
+from typing import List, Union
 
-from nltk.tokenize import sent_tokenize
-
-
-def tokenize_into_sentences(text: str) -> List[str]:
-    return sent_tokenize(text)
+from gensim.models import KeyedVectors, Word2Vec
 
 
-def normalize_sentence(sentence: str) -> str:
+def preprocess(
+    sentences: List[str],
+    remove_punctuation: bool = True,
+    remove_digits: bool = True,
+    remove_chars: str = "",
+    lowercase: bool = True,
+    strip: bool = True,
+    remove_whitespaces: bool = True,
+) -> List[str]:
     """
-    Normalize a sentence.
-    
-    The function will perform several steps: l
-    - remove punctuation and digits
-    - lower the case, strip and remove superfluous white spaces
-    
-    >>> normalize_sentence(" Return the factorial of n, an  exact integer >= 0.")
-    'return the factorial of n an exact integer'
+    Preprocess a list of sentences for word embedding.
+
+    Args:
+        sentence: list of sentences
+        remove_punctuation: whether to remove string.punctuation
+        remove_digits: whether to remove string.digits
+        remove_chars: remove the given characters
+        lowercase: whether to lower the case
+        strip: whether to strip
+        remove_whitespaces: whether to remove superfluous whitespaceing by " ".join(str.split(())
+    Returns:
+        Processed list of sentences
+
+    Examples:
+        >>> preprocess([' Return the factorial of n, an  exact integer >= 0.'])
+        ['return the factorial of n an exact integer']
+        >>> preprocess(['A1b c\\n\\nde \\t fg\\rkl\\r\\n m+n'])
+        ['ab c de fg kl mn']
     """
-    # remove punctuation and digits
-    sentence = re.sub(f"[{string.punctuation}+{string.digits}]", "", sentence)
+    # remove chars
+    if remove_punctuation is True:
+        remove_chars += string.punctuation
+    if remove_digits is True:
+        remove_chars += string.digits
+    if remove_chars:
+        sentences = [re.sub(f"[{remove_chars}]", "", sent) for sent in sentences]
 
-    # lower the case, strip and remove superfluous white spaces
-    sentence = " ".join(sentence.lower().strip().split())
-    return sentence
+    # lowercase, strip and remove superfluous white spaces
+    if lowercase:
+        sentences = [sent.lower() for sent in sentences]
+    if strip:
+        sentences = [sent.strip() for sent in sentences]
+    if remove_whitespaces:
+        sentences = [" ".join(sent.split()) for sent in sentences]
+    return sentences
 
 
-def preprocess(text: str) -> List[str]:
-    sentences = tokenize_into_sentences(text)
-    return [normalize_sentence(sentence) for sentence in sentences]
+def run_word2vec(
+    sentences: List[str],
+    model: Word2Vec,
+    pretrained_path: str,
+    path: Union[None, str] = None,
+) -> Union[None, Word2Vec]:
+    model.build_vocab(sentences)
+    total_examples = model.corpus_count
+    pretrained_model = KeyedVectors.load_word2vec_format(pretrained_path, binary=False)
+    model.build_vocab([list(pretrained_model.vocab.keys())], update=True)
+    model.intersect_word2vec_format(pretrained_path, binary=False, lockf=1.0)
+    model.train(sentences, total_examples=total_examples, epochs=model.iter)
+
+    if path is not None:
+        model.save(path)
+        return None
+    else:
+        return model
+
+
+def phrase_embeddings(model: Union[str, Word2Vec]):
+    # TODO
+    if isinstance(model, str):
+        model = Word2Vec.load(model)
+    elif isinstance(model, str):
+        pass
+    else:
+        raise TypeError("model is either the a string or an Word2Vec object")
+
+    # Create a word count dictionary based on the trained model
+    word_count_dict = {}
+    for word, vocab_obj in model.wv.vocab.items():
+        word_count_dict[word] = vocab_obj.count
+
+    # Create a dictionary that maps from a word to its frequency and then use the frequency of
+    # a word to compute its sif-weight (saved in sif_dict)
+    sif_dict = {}
+    for word, count in word_count_dict.items():
+        sif_dict[word] = 0.001 / (0.001 + count)
+    # pk.dump(sif_dict, open('sif_dict.p', 'wb'))
