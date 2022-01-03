131742567fe18900b6f92eb6832bedea883c8c05
PinchOfData <germain.gauthier@polytechnique.edu>
2022-01-03 15:17:22 +0100

    class-oriented version of the package

---
+++ relatio/narrative_models.py
@@ -0,0 +1,468 @@
+from abc import ABC, abstractmethod
+from typing import Type
+from collections import Counter
+from scipy.spatial.distance import cdist
+from tqdm import tqdm
+
+from .embeddings import *
+
+
+class NarrativeModelBase(ABC):
+    """
+
+    A general class to build a model that extracts latent narratives from a list of SRL statements.
+
+    """
+
+    def __init__(
+        self,
+        roles_considered: List[str] = ["ARG0", "B-V", "ARGM-MOD", "ARG1", "ARG2"],
+        roles_with_entities: str = ["ARG0", "ARG1", "ARG2"],
+        list_of_known_entities: Optional[List[str]] = None,
+        assignment_to_known_entities: str = "regex",
+        roles_with_embeddings: List[List[str]] = [["ARG0", "ARG1", "ARG2"]],
+        embeddings_model: Optional[Type[Embeddings]] = None,
+        threshold: int = 0.1,
+    ):
+
+        self.roles_considered = roles_considered
+        self.roles_with_embeddings = roles_with_embeddings
+        self.roles_with_entities = roles_with_entities
+        self.list_of_known_entities = list_of_known_entities
+        self.vectors_of_known_entities = None
+        self.assignment_to_known_entities = assignment_to_known_entities
+        self.threshold = threshold
+
+        if embeddings_model is None:
+            self.embeddings_model = Embeddings("spaCy", "en_core_web_sm")
+        else:
+            self.embeddings_model = embeddings_model
+
+        if self.list_of_known_entities is not None:
+            if self.assignment_to_known_entities == "embeddings":
+                for i, entity in enumerate(self.list_of_known_entities):
+                    vector = self.embeddings_model.get_vector(entity)
+                    if i == 0:
+                        self.vectors_of_known_entities = np.array([vector])
+                    else:
+                        self.vectors_of_known_entities = np.append(
+                            self.vectors_of_known_entities, [vector], axis=0
+                        )
+
+        self.vectors = []
+        self.labels = []
+        self.vocab = []
+        for l in roles_with_embeddings:
+            self.vectors.append([])
+            self.labels.append({})
+            self.vocab.append({})
+
+    @abstractmethod
+    def train(self, list_of_srl_res):
+        pass
+
+    def _process_srl_item_for_prediction(self, role, content):
+
+        if role in self.roles_considered:
+
+            if role in ["B-V", "B-ARGM-NEG", "B-ARGM-MOD"]:
+                return content
+
+            elif role in ["ARG0", "ARG1", "ARG2"]:
+
+                # Character matching of known entities
+                if role in self.roles_with_entities:
+                    if self.assignment_to_known_entities == "regex":
+                        for e in self.list_of_known_entities:
+                            if e in content:
+                                return e
+
+                # Clustering with embeddings
+                for i, l in enumerate(self.roles_with_embeddings):
+                    if role in l:
+                        vector = self.embeddings_model.get_vector(content)
+                        if len(vector) != 0:
+
+                            # Known entities
+                            if role in self.roles_with_entities:
+                                if self.assignment_to_known_entities == "embeddings":
+                                    distances = cdist(
+                                        self.vectors_of_known_entities,
+                                        vector.reshape(1, -1),
+                                        metric="euclidean",
+                                    ).flatten()
+                                    nmin = min(distances)
+                                    if nmin <= self.threshold:
+                                        entity_index = np.where(
+                                            distances == np.amin(distances)
+                                        )[0][0]
+                                        return self.list_of_known_entities[entity_index]
+
+                            # Unknown entities
+                            if len(self.vectors[i]) != 0:
+                                distances = cdist(
+                                    self.vectors[i],
+                                    vector.reshape(1, -1),
+                                    metric="euclidean",
+                                ).flatten()
+                                nmin = min(distances)
+                                if nmin <= self.threshold:
+                                    clu = np.where(distances == np.amin(distances))[0][
+                                        0
+                                    ]
+                                    return self.labels[i][clu]
+
+    def get_element(self, narrative, role):
+        return narrative[role] if role in narrative else ""
+
+    def prettify(self, narrative):
+
+        ARG0 = self.get_element(narrative, "ARG0")
+        V = self.get_element(narrative, "B-V")
+
+        NEG = self.get_element(narrative, "B-ARGM-NEG")
+        if NEG:
+            NEG = "not"
+        else:
+            NEG == ""
+
+        MOD = self.get_element(narrative, "B-ARGM-MOD")
+        ARG1 = self.get_element(narrative, "ARG1")
+        ARG2 = self.get_element(narrative, "ARG2")
+
+        pretty_narrative = (ARG0, NEG, V, MOD, ARG1, ARG2)
+
+        pretty_narrative = " ".join([t for t in pretty_narrative if t != ""])
+
+        return pretty_narrative
+
+    def predict(
+        self, list_of_srl_res, prettify: bool = False, progress_bar: bool = False
+    ):
+
+        """
+
+        Predict the latent narrative statement based of a SRL statement.
+
+        """
+
+        if progress_bar:
+            list_of_srl_res = tqdm(list_of_srl_res)
+
+        list_of_narratives = []
+        for srl_res in list_of_srl_res:
+            narrative = {}
+            for role, content in srl_res.items():
+                pred = self._process_srl_item_for_prediction(role, content)
+                if pred is not None:
+                    narrative[role] = pred
+            list_of_narratives.append(narrative)
+
+        if prettify:
+            list_of_narratives = [
+                self.prettify(narrative) for narrative in list_of_narratives
+            ]
+
+        return list_of_narratives
+
+
+class DeterministicModel(NarrativeModelBase):
+    """
+
+    A subclass of NarrativeModel(), which does nothing more.
+
+    """
+
+    def train(self, list_of_srl_res):
+        print("No training required: the model is deterministic.")
+
+
+from relatio.utils import count_values
+from sklearn.cluster import KMeans
+import warnings
+
+
+class StaticModel(NarrativeModelBase):
+    """
+
+    A subclass of NarrativeModel(), which mines K latent entities via a one-off clustering algorithm
+    on a training set (e.g., K-Means).
+
+    """
+
+    def __init__(
+        self,
+        roles_considered,
+        roles_with_entities,
+        list_of_known_entities,
+        assignment_to_known_entities,
+        roles_with_embeddings,
+        embeddings_model,
+        threshold: int,
+        n_clusters: List[int],
+    ):
+
+        super().__init__(
+            roles_considered,
+            roles_with_entities,
+            list_of_known_entities,
+            assignment_to_known_entities,
+            roles_with_embeddings,
+            embeddings_model,
+            threshold,
+        )
+
+        self.n_clusters = n_clusters
+        self.clustering_models = []
+        self.training_vectors = []
+
+        for l in roles_with_embeddings:
+            self.clustering_models.append({})
+            self.training_vectors.append({})
+
+    def _remove_entities(self, list_of_phrases: List[str]):
+
+        if self.assignment_to_known_entities == "regex":
+            for phrase in list_of_phrases:
+                for entity in self.list_of_known_entities:
+                    if is_subsequence(entity.split(), phrase.split()):
+                        list_of_phrases.remove(phrase)
+        else:
+            for phrase in list_of_phrases:
+                vector = self.embeddings_model.get_vector(phrase)
+                if len(vector) != 0:
+                    distances = cdist(
+                        self.vectors_of_known_entities,
+                        vector.reshape(1, -1),
+                        metric="euclidean",
+                    ).flatten()
+                    nmin = min(distances)
+                    if nmin <= self.threshold:
+                        list_of_phrases.remove(phrase)
+
+        return list_of_phrases
+
+    def train(
+        self,
+        list_of_srl_res,
+        random_state: int = 1,
+        verbose: int = 0,
+        max_iter: int = 300,
+        progress_bar: bool = False,
+    ):
+
+        for i, roles in enumerate(self.roles_with_embeddings):
+
+            if progress_bar:
+                print("Focus on roles: %s" % "-".join(roles))
+                print("Ignoring known entities...")
+
+            phrases_to_embed = []
+            counter_for_phrases = Counter()
+            for role in roles:
+                temp_counter = count_values(list_of_srl_res, keys=[role])
+                counter_for_phrases = counter_for_phrases + temp_counter
+                phrases = list(temp_counter)
+                if role in self.roles_with_entities:
+                    phrases = self._remove_entities(phrases)
+                phrases_to_embed.extend(phrases)
+
+            phrases_to_embed = list(set(phrases_to_embed))
+
+            if progress_bar:
+                print("Embedding relevant phrases...")
+
+            vecs = []
+            for phrase in phrases_to_embed:
+                vec = self.embeddings_model.get_vector(phrase)
+                if len(vec) == 0:
+                    phrases_to_embed.remove(phrase)
+                else:
+                    vec = np.array([vec])  # discuss with Andrei
+                    if vec is not None:
+                        vecs.append(vec)
+
+            vecs = np.concatenate(vecs)
+            self.training_vectors[i] = vecs
+
+            if progress_bar:
+                print("Clustering phrases into %s clusters..." % self.n_clusters[i])
+
+            kmeans = KMeans(
+                n_clusters=self.n_clusters[i],
+                random_state=random_state,
+                verbose=verbose,
+                max_iter=max_iter,
+            ).fit(self.training_vectors[i])
+
+            self.clustering_models[i] = kmeans
+            self.vectors[i] = kmeans.cluster_centers_
+
+            if progress_bar:
+                print("Labeling the clusters by the most frequent phrases...")
+
+            for clu in range(self.n_clusters[i]):
+                self.vocab[i][clu] = Counter()
+            for j, clu in enumerate(kmeans.labels_):
+                self.vocab[i][clu][phrases_to_embed[j]] = counter_for_phrases[
+                    phrases_to_embed[j]
+                ]
+            for clu in range(self.n_clusters[i]):
+                token_most_common = self.vocab[i][clu].most_common(2)
+                if len(token_most_common) > 1 and (
+                    token_most_common[0][1] == token_most_common[1][1]
+                ):
+                    warnings.warn(
+                        f"Multiple labels for cluster {clu}- 2 shown: {token_most_common}. First one is picked.",
+                        RuntimeWarning,
+                    )
+                self.labels[i][clu] = token_most_common[0][0]
+
+
+class DynamicModel(NarrativeModelBase):
+    """
+
+    A subclass of NarrativeModel(), which mines latent entities on-the-fly based on a dynamic clustering algorithm.
+
+    """
+
+    def __init__(
+        self,
+        roles_considered,
+        roles_with_entities,
+        list_of_known_entities,
+        assignment_to_known_entities,
+        roles_with_embeddings,
+        embeddings_model,
+        threshold: int = 0.1,
+    ):
+
+        super().__init__(
+            roles_considered,
+            roles_with_entities,
+            list_of_known_entities,
+            assignment_to_known_entities,
+            roles_with_embeddings,
+            embeddings_model,
+            threshold,
+        )
+
+    def _process_srl_item_for_training(self, role, content):
+
+        if role in ["ARG0", "ARG1", "ARG2"]:
+            if role in self.roles_considered:
+
+                # Character matching of known entities (skipped for training)
+                if role in self.roles_with_entities:
+                    if self.assignment_to_known_entities == "regex":
+                        for e in self.list_of_known_entities:
+                            if e in content:
+                                return None
+
+                # Clustering with embeddings
+                for i, l in enumerate(self.roles_with_embeddings):
+                    if role in l:
+                        vector = self.embeddings_model.get_vector(content)
+                        if len(vector) != 0:
+
+                            # Known entities (skipped for training)
+                            if role in self.roles_with_entities:
+                                if self.assignment_to_known_entities == "embeddings":
+                                    distances = cdist(
+                                        self.vectors_of_known_entities,
+                                        vector.reshape(1, -1),
+                                        metric="euclidean",
+                                    ).flatten()
+                                    nmin = min(distances)
+                                    if nmin <= self.threshold:
+                                        return None
+
+                            # Unknown entities
+                            if len(self.vectors[i]) == 0:
+                                self.vectors[i] = np.array([vector])
+                                self.vocab[i][0] = Counter()
+                                self.vocab[i][0][content] = 1
+                                self.labels[i][0] = content
+                            else:
+                                clu = len(self.vectors[i])
+                                distances = cdist(
+                                    self.vectors[i],
+                                    vector.reshape(1, -1),
+                                    metric="euclidean",
+                                ).flatten()
+                                nmin = min(distances)
+                                if nmin <= self.threshold:
+                                    clu = np.where(distances == np.amin(distances))[0][
+                                        0
+                                    ]
+                                    self.vocab[i][clu][content] += 1
+
+                                    token_most_common = self.vocab[i][clu].most_common(
+                                        2
+                                    )
+                                    if len(token_most_common) > 1 and (
+                                        token_most_common[0][1]
+                                        == token_most_common[1][1]
+                                    ):
+                                        warnings.warn(
+                                            f"Multiple labels for cluster {clu}- 2 shown: {token_most_common}. First one is picked.",
+                                            RuntimeWarning,
+                                        )
+                                    self.labels[i][clu] = token_most_common[0][0]
+                                else:
+                                    self.vectors[i] = np.append(
+                                        self.vectors[i], [vector], axis=0
+                                    )
+                                    self.vocab[i][clu] = Counter()
+                                    self.vocab[i][clu][content] = 1
+                                    self.labels[i][clu] = content
+
+    def train(self, list_of_srl_res, progress_bar: bool = False):
+
+        if progress_bar:
+            list_of_srl_res = tqdm(list_of_srl_res)
+
+        for srl_res in list_of_srl_res:
+            for role, content in srl_res.items():
+                self._process_srl_item_for_training(role, content)
+
+
+class NarrativeModel(NarrativeModelBase):
+    def __init__(
+        self,
+        model_type,
+        roles_considered: List[str] = ["ARG0", "B-V", "ARGM-MOD", "ARG1", "ARG2"],
+        roles_with_entities: str = ["ARG0", "ARG1", "ARG2"],
+        list_of_known_entities: List[str] = [],
+        assignment_to_known_entities: str = "regex",
+        roles_with_embeddings: List[str] = [["ARG0", "ARG1", "ARG2"]],
+        embeddings_model: Optional[Type[Embeddings]] = None,
+        threshold: int = 0.1,
+        **kwargs,
+    ):
+
+        if model_type == "dynamic":
+            _MODEL_CLASS = DynamicModel
+        elif model_type == "static":
+            _MODEL_CLASS = StaticModel
+        elif model_type == "deterministic":
+            _MODEL_CLASS = DeterministicModel
+
+        self._model_obj = _MODEL_CLASS(
+            roles_considered,
+            roles_with_entities,
+            list_of_known_entities,
+            assignment_to_known_entities,
+            roles_with_embeddings,
+            embeddings_model,
+            threshold,
+            **kwargs,
+        )
+
+    def train(self, list_of_srl_res, **kwargs):
+        self._model_obj.train(list_of_srl_res, **kwargs)
+
+    def predict(
+        self, list_of_srl_res, prettify: bool = False, progress_bar: bool = False
+    ):
+        return self._model_obj.predict(list_of_srl_res, prettify, progress_bar)
---
+++ relatio/preprocessing.py
@@ -0,0 +1,311 @@
+from collections import Counter
+from scipy.spatial.distance import cdist
+from sklearn.preprocessing import normalize
+import numpy as np
+import spacy
+import time
+import pandas as pd
+from copy import deepcopy
+from tqdm import tqdm
+from typing import Dict, List, Optional, Tuple, Union, Any
+from spacy.lang.en.stop_words import STOP_WORDS as en_stop
+
+en_stop = list(en_stop)
+
+
+class Preprocessor:
+    """
+
+    A class to preprocess a given corpus
+
+    (e.g., split it into sentences, annotate semantic roles, clean the text, mine named entities)
+
+    """
+
+    def __init__(self, spacy_model):
+        self.spacy_model = spacy_model
+        self.nlp = spacy.load(spacy_model)
+
+    def split_into_sentences(
+        self,
+        dataframe: pd.DataFrame,
+        output_path: Optional[str] = None,
+        progress_bar: bool = False,
+    ) -> Tuple[List[str], List[str]]:
+
+        """
+
+        Split a list of documents into sentences (using the SpaCy sentence splitter).
+
+        Args:
+            dataframe: a pandas dataframe with one column "id" and one column "doc"
+            output_path: path to save the output
+            progress_bar: print a progress bar (default is False)
+
+        Returns:
+            Tuple with the list of document indices and list of sentences
+
+        """
+
+        docs = dataframe.to_dict(orient="records")
+
+        sentences: List[str] = []
+        doc_indices: List[str] = []
+
+        if progress_bar:
+            print("Splitting into sentences...")
+            time.sleep(1)
+            docs = tqdm(docs)
+
+        for doc in docs:
+            for sent in self.nlp(doc["doc"], disable=["tagger", "ner"]).sents:
+                sentences.append(str(sent))
+                doc_indices = doc_indices + [doc["id"]]
+
+        if output_path is not None:
+            with open(output_path, "w") as f:
+                json.dump((doc_indices, sentences), f)
+
+        return (doc_indices, sentences)
+
+    def mine_entities(
+        self,
+        sentences: List[str],
+        ent_labels: Optional[List[str]] = ["PERSON", "NORP", "ORG", "GPE", "EVENT"],
+        progress_bar: bool = False,
+    ) -> Counter:
+
+        """
+
+        Go through sentences and counts named entities found in the corpus.
+
+        Args:
+            sentences: list of sentences
+            ent_labels: list of entity labels to be considered (see SpaCy documentation)
+            progress_bar: print a progress bar (default is False)
+            For other arguments see utils.clean_text.
+
+        Returns:
+            Counter with the named entity and its associated frequency on the corpus
+
+        """
+
+        entities_all = []
+
+        if progress_bar:
+            print("Mining named entities...")
+            time.sleep(1)
+            sentences = tqdm(sentences)
+
+        for sentence in sentences:
+            sentence = self.nlp(sentence)
+            for ent in sentence.ents:
+                if ent.label_ in ent_labels:
+                    entities_all.append(ent.text)
+
+        entity_counts = Counter(entities_all)
+
+        return entity_counts
+
+    def clean_text(
+        self,
+        sentence: str,
+        remove_punctuation: bool = True,
+        remove_digits: bool = True,
+        stop_words: List[str] = [],
+        lowercase: bool = True,
+        lemmatize: bool = True,
+        pos_tags_to_keep: Optional[List[str]] = None,
+    ) -> List[str]:
+
+        """
+
+        Clean a string of text.
+
+        """
+
+        s = self.nlp(sentence)
+
+        if remove_punctuation:
+            s = [t for t in s if t.is_punct == False]
+
+        if remove_digits:
+            s = [t for t in s if t.is_digit == False]
+
+        if pos_tags_to_keep:
+            s = [t for t in s if t.pos_ in pos_tags_to_keep]
+
+        if lowercase and not lemmatize:
+            s = [t.lower_ for t in s]
+
+        if lowercase and lemmatize:
+            s = [t.lemma_.lower() for t in s]
+
+        if not lowercase and not lemmatize:
+            s = [t.text for t in s]
+
+        s = [t for t in s if t not in stop_words]
+
+        s = [t.strip() for t in s if t not in stop_words]
+
+        s = " ".join(s)
+
+        return s
+
+    def extract_role_per_sentence(
+        self, sentence_dict: dict, used_roles: List[str]
+    ) -> List[Dict[str, Union[str, bool]]]:
+
+        """
+
+        Extract the semantic roles for a given sentence.
+
+        Args:
+            srl: srl output
+            used_roles: list of semantic roles to extract
+
+        Returns:
+            List of statements with their associated roles for a given sentence
+
+        """
+
+        word_list = sentence_dict["words"]
+        sentence_role_list = []
+
+        for statement_dict in sentence_dict["verbs"]:
+            tag_list = statement_dict["tags"]
+
+            statement_role_dict: Dict[str, Union[str, bool]] = {}
+            for role in ["ARG0", "ARG1", "ARG2", "B-V", "B-ARGM-MOD"]:
+                if role in used_roles:
+                    indices_role = [i for i, tok in enumerate(tag_list) if role in tok]
+                    toks_role = [
+                        tok for i, tok in enumerate(word_list) if i in indices_role
+                    ]
+                    statement_role_dict[role] = " ".join(toks_role)
+
+            if "B-ARGM-NEG" in used_roles:
+                role_negation_value = any("B-ARGM-NEG" in tag for tag in tag_list)
+                statement_role_dict["B-ARGM-NEG"] = role_negation_value
+
+            key_to_delete = []
+            for key, value in statement_role_dict.items():
+                if not value:
+                    key_to_delete.append(key)
+            for key in key_to_delete:
+                del statement_role_dict[key]
+            sentence_role_list.append(statement_role_dict)
+
+        if not sentence_role_list:
+            sentence_role_list = [{}]
+
+        return sentence_role_list
+
+    def extract_roles(
+        self,
+        srl: List[Dict[str, Any]],
+        used_roles: List[str],
+        progress_bar: bool = False,
+    ) -> Tuple[List[Dict[str, Union[str, bool]]], List[int]]:
+
+        """
+
+        Extract semantic roles from the SRL output.
+
+        Args:
+            srl: srl output
+            used_roles: list of semantic roles to extract
+            progress_bar: print a progress bar (default is False)
+
+        Returns:
+            List of statements and numpy array of sentence indices (to keep track of sentences)
+
+        """
+
+        statements_role_list: List[Dict[str, Union[str, bool]]] = []
+        sentence_index: List[int] = []
+
+        if progress_bar:
+            print("Extracting semantic roles...")
+            time.sleep(1)
+            srl = tqdm(srl)
+
+        for i, sentence_dict in enumerate(srl):
+            role_per_sentence = self.extract_role_per_sentence(
+                sentence_dict, used_roles
+            )
+            sentence_index.extend([i] * len(role_per_sentence))
+            statements_role_list.extend(role_per_sentence)
+
+        return statements_role_list, np.asarray(sentence_index, dtype=np.uint32)
+
+    def process_roles(
+        self,
+        statements: List[Dict[str, List]],
+        max_length: Optional[int] = None,
+        remove_punctuation: bool = True,
+        remove_digits: bool = True,
+        stop_words: List[str] = [],
+        lowercase: bool = True,
+        lemmatize: bool = True,
+        dict_of_pos_tags_to_keep: Optional[dict] = None,
+        progress_bar: bool = False,
+    ) -> List[Dict[str, List]]:
+
+        """
+
+        Takes a list of raw extracted semantic roles and cleans the text.
+
+        Args:
+            max_length = remove roles of more than n characters (NB: very long roles tend to be uninformative)
+            progress_bar: print a progress bar (default is False)
+            For other arguments see utils.clean_text.
+
+        Returns:
+            List of processed statements
+
+        """
+
+        roles_copy = deepcopy(statements)
+
+        if progress_bar:
+            print("Cleaning semantic roles...")
+            time.sleep(1)
+            statements = tqdm(statements)
+
+        pos_tags_to_keep = {
+            "ARG0": None,
+            "ARG1": None,
+            "ARG2": None,
+            "B-ARGM-MOD": None,
+            "B-V": None,
+        }
+        if dict_of_pos_tags_to_keep is not None:
+            for role in dict_of_pos_tags_to_keep.keys():
+                pos_tags_to_keep[role] = dict_of_pos_tags_to_keep[role]
+
+        for i, statement in enumerate(statements):
+            for role, role_content in roles_copy[i].items():
+                if isinstance(role_content, str):
+                    res = self.clean_text(
+                        role_content,
+                        remove_punctuation=remove_punctuation,
+                        remove_digits=remove_digits,
+                        stop_words=stop_words,
+                        lowercase=lowercase,
+                        lemmatize=lemmatize,
+                        pos_tags_to_keep=pos_tags_to_keep[role],
+                    )
+                    if max_length is not None:
+                        if len(res) <= max_length:
+                            roles_copy[i][role] = res
+                        else:
+                            roles_copy[i][role] = ""
+                    else:
+                        roles_copy[i][role] = res
+                elif isinstance(role_content, bool):
+                    pass
+                else:
+                    raise ValueError(f"{role_content}")
+
+        return roles_copy
